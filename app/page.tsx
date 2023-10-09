8bb9310351d6315a7166614c979b59ab38f3b58b
Michael Deng <michaeldeng55@gmail.com>
2023-10-09 00:47:21 -0400

    success query the db

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import {useEffect} from 'react' 
+import 'dotenv/config'
 
 const page = () => {
+
+  const getData = async () => {
+    fetch(`https://api.spoonacular.com/recipes/1003464/ingredientWidget.json?apiKey=${process.env.API_KEY}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch((err) => {
+      console.error(err);
+      
+    })
+  }
+
+  // getData()
+
   return (
-    <div>page</div>
+    <div className='p-10'>Testing for food API responsivity.</div>
   )
 }
 
